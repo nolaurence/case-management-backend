09cdf0322e8fc64aee1b71f24851f2a672b8b2ed
nolaurence <nolaurence@outlook.com>
2023-10-30 23:33:16 +0800

    first commit

---
+++ src/main/java/cn/nolaurene/casemanagementbackend/CaseManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.nolaurene.casemanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/nolaurene/casemanagementbackend/CaseManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nolaurene.casemanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
